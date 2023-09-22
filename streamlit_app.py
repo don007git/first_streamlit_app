f74ee1b6e2c7481a3099dc4bc62efbe8ce35d521
don007git <dqli007@gmail.com>
2023-09-22 09:47:38 -0500

    Update streamlit_app.py
    use suggested variable names in case they are used by Dora for testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,9 @@ myfruit_df = pd.read_csv(myfile)
 # Let's put a pick list here so they can pick the fruit they want to include 
 myfruit_df = myfruit_df.set_index("Fruit")
 my_default_selections = ['Avocado','Strawberries']
-myselections = streamlit.multiselect("Pick some fruits:", list(myfruit_df.index), my_default_selections)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(myfruit_df.index), my_default_selections)
 
 # Display myselections on the page.
-streamlit.dataframe(myfruit_df.loc[myselections])
+fruits_to_show = myfruit_df.loc[myselections]
+streamlit.dataframe(fruits_to_show)
 
