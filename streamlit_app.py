bded6932443739b7a633737ac65f942c53133ff0
don007git <dqli007@gmail.com>
2023-09-22 09:31:30 -0500

    Update streamlit_app.py
    add fruit picker for users

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,11 @@ import pandas as pd
 myfile = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
 myfruit_df = pd.read_csv(myfile)
 # print(mymyfruit_df.head(3))
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+mymyfruit_df.set_index("Fruit")
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(myfruit_df)
 
