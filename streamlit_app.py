305898f2372f91ca5fc150dedbd02962956d3741
don007git <dqli007@gmail.com>
2023-09-22 09:28:33 -0500

    Update streamlit_app.py
    fix bug by comment print statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas as pd
 myfile = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
 myfruit_df = pd.read_csv(myfile)
-print(mymyfruit_df.head(3))
+# print(mymyfruit_df.head(3))
 streamlit.dataframe(myfruit_df)
 
